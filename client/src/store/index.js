79941a439f894c9ed67e973c4ca2541e750e34f3
季娜(平安科技企业大数据团队实施组) <jina194@C02T8D1GGY25.local>
2018-05-24 16:44:36 +0800

    删除不必要的依赖

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,12 @@
 import { observable, computed, autorun, action } from 'mobx';
 
 export default class AppState {
-  @observable count = 0; //类的实例属性可以用等式写入类的定义之中
+  @observable count = 0; // 类的实例属性可以用等式写入类的定义之中
   @observable name = 'grace';
-  @computed get msg() {
+  @computed get msg () {
     return `${this.name} say count is ${this.count}`;
   };
-  @action add() {
+  @action add () {
     this.count += 2;
   };
 }
